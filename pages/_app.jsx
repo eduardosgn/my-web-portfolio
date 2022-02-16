221c399402f0e9d82f670be1a6710095276ef039
unknown <you@example.com>
2022-02-16 13:51:57 -0300

    Ajuste na _app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,38 +1,39 @@
-
 import Head from 'next/head';
 
 import '../styles/globals.css';
-import { motion, AnimatePresence } from 'framer-motion';
+import { AnimatePresence, LayoutGroup, motion } from 'framer-motion';
 
 function MyApp({ Component, pageProps, router }) {
   return (
-    <AnimatePresence exitBeforeEnter>
-      <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      </Head>
-      <motion.div key={router.route} initial='pageInitial' animate='pageAnimate' exit='pageExit' variants={{
-        pageInitial: {
-          opacity: 0,
-        },
-        pageAnimate: {
-          opacity: 1,
-          transition: {
-            duration: .6
-          }
-        },
-        pageExit: {
-          opacity: 0,
-          transition: {
-            duration: .4
-          }
-        },
-      }}>
-        <Component {...pageProps} />
-      </motion.div>
-    </AnimatePresence>
+    <LayoutGroup>
+      <AnimatePresence exitBeforeEnter>
+        <Head>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        </Head>
+        <motion.div key={router.route} initial='pageInitial' animate='pageAnimate' exit='pageExit' variants={{
+          pageInitial: {
+            opacity: 0,
+          },
+          pageAnimate: {
+            opacity: 1,
+            transition: {
+              duration: .6
+            }
+          },
+          pageExit: {
+            opacity: 0,
+            transition: {
+              duration: .4
+            }
+          },
+        }}>
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
+    </LayoutGroup>
   );
 };
 
