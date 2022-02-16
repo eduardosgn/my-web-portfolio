eda6e690290bf463807818df1e6704aa575f8d4e
unknown <you@example.com>
2022-02-16 18:24:14 -0300

    Bugfix nas transições das páginas

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,36 +3,43 @@ import Head from 'next/head';
 import '../styles/globals.css';
 import { AnimatePresence, motion } from 'framer-motion';
 
-import Router from "next/router";
+import { useTransitionFix } from '../utils/useTransitionFix.ts';
 
-const routeChange = () => {
-  // Temporary fix to avoid flash of unstyled content
-  // during route transitions. Keep an eye on this
-  // issue and remove this code when resolved:
-  // https://github.com/vercel/next.js/issues/17464
+// import Router from "next/router";
 
-  const tempFix = () => {
-    const allStyleElems = document.querySelectorAll('style[media="x"]');
-    allStyleElems.forEach((elem) => {
-      elem.removeAttribute("media");
-    });
-  };
-  tempFix();
-};
+// const routeChange = () => {
+//   // Temporary fix to avoid flash of unstyled content
+//   // during route transitions. Keep an eye on this
+//   // issue and remove this code when resolved:
+//   // https://github.com/vercel/next.js/issues/17464
 
-Router.events.on("routeChangeComplete", routeChange );
-Router.events.on("routeChangeStart", routeChange );
+//   const tempFix = () => {
+//     const allStyleElems = document.querySelectorAll('style[media="x"]');
+//     allStyleElems.forEach((elem) => {
+//       elem.removeAttribute("media");
+//     });
+//   };
+//   tempFix();
+// };
+
+// Router.events.on("routeChangeComplete", routeChange );
+// Router.events.on("routeChangeStart", routeChange );
 
 function MyApp({ Component, pageProps, router }) {
   return (
-    <AnimatePresence exitBeforeEnter>
+    <AnimatePresence exitBeforeEnter onExitComplete={ useTransitionFix }>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </Head>
-      <motion.div key={router.route} initial='pageInitial' animate='pageAnimate' exit='pageExit' variants={{
+      <motion.div 
+        key={router.route} 
+        initial='pageInitial' 
+        animate='pageAnimate' 
+        exit='pageExit' 
+        variants={{
           pageInitial: {
             opacity: 0,
           },
