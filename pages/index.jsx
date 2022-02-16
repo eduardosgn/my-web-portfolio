edd7a76ca047c85de32ab3bae00d35c311f14590
unknown <you@example.com>
2022-02-16 14:05:09 -0300

    Alterações na page index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,10 @@
 import Header from '../components/Header/Header';
+import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
-    <>
+    <motion.div>
       <Header />
-    </>
+    </motion.div>
   );
 };
